71f173f62513e85ee0bcc9cef1d1c4f2edcc3602
mario <mrestori64@gmail.com>
2026-03-12 11:38:40 -0700

    Create custom song update.rb

---
+++ custom song update.rb
@@ -0,0 +1,184 @@
+#Iron man black sabbath
+use_bpm 117
+lyricPlay= ["nobody", "want", "him", "he", "just", "stares", "at", "the", "world"]
+l = 0
+iron_man = "C:/Users/mario_restori/Documents/Audacity/i_am_iron_man.wav"
+define :outro_part do
+  2.times do
+    play :e4
+    sleep 0.5
+    play :d4
+    sleep 0.5
+    play :e4, sustain: 2.5
+    sleep 2.5
+    play :d4
+    sleep 0.5
+    play :e4
+    sleep 0.5
+    play :d4, sustain: 4
+    sleep 4
+    play :e4
+    sleep 0.5
+    play :d4
+    sleep 0.5
+    play :e4, sustain: 2.5
+    sleep 2.5
+    play :f4
+    sleep 0.45
+    play :e4
+    sleep 0.45
+    play :g4
+    sleep 0.45
+    play :f4
+    sleep 0.45
+    play :a4
+    sleep 0.45
+    play :c4, sustain: 1
+    sleep 1
+    play :a4
+    sleep 0.5
+    play :e4
+    sleep 0.5
+    play :d4
+    sleep 0.5
+    play :e4
+  end
+  stop
+end
+live_loop :drums do
+  use_synth :sc808_bassdrum
+  play :a1, amp: 0.5
+  sleep 2
+  play :a1, amp: 0.5
+  sleep 2
+  play :a1, amp: 0.5
+  sleep 2
+  play :a1, amp: 0.5
+  sleep 2
+end
+sleep 8
+use_synth :pluck
+play :d4, sustain: 3
+sleep 4
+play :g3, sustain: 3
+sleep 1
+sample iron_man
+sleep 6
+2.times do
+  use_synth :pluck
+  play :d4, sustain: 3
+  sleep 4
+  play :g3, sustain: 3
+  sleep 6
+end
+live_loop :guitar do
+  4.times do
+    use_synth :pluck
+    play :A3, amp: 2
+    sleep 1.75
+    play :C4, amp: 2
+    sleep 1.5
+    play :C4, amp: 2
+    sleep 0.85
+    play :D4, amp: 2
+    sleep 0.75
+    play :D4, amp: 2
+    sleep 1.75
+    play :f4, amp: 2
+    sleep 0.4
+    play :e4, amp: 2
+    sleep 0.4
+    play :f4, amp: 2
+    sleep 0.4
+    play :e4, amp: 2
+    sleep 0.4
+    play :f4, amp: 2
+    sleep 0.4
+    play :e4, amp: 2
+    sleep 0.4
+    play :c4, amp: 2
+    sleep 0.85
+    play :c4, amp: 2
+    sleep 0.85
+    play :D4, amp: 2
+    sleep 0.75
+    play :d4, amp: 2
+    sleep 1.5
+  end
+  stop
+end
+sleep 54
+9.times do
+  print(lyricPlay[l])
+  sleep 1
+  l = l + 1
+end
+live_loop :melody_2 do
+  4.times do
+    play :a3
+    sleep 0.75
+    play :a3
+    sleep 1.5
+    play :c4
+    sleep 1.5
+    play :a3
+    sleep 1.35
+    play :Ab3
+    sleep 0.8
+    play :g3
+    sleep 0.8
+    play :g3
+    sleep 0.8
+    play :g3
+    sleep 1.5
+    play :fs3
+    sleep 0.75
+    play :g3
+    sleep 0.75
+    play :g3
+    sleep 0.75
+    play :ab3
+    sleep 1
+  end
+  stop
+end
+sleep 54
+
+outro_part
+play :e4
+sleep 0.5
+play :d4
+sleep 0.5
+play :e4, sustain: 2.5
+sleep 2.5
+play :d4
+sleep 0.5
+play :e4
+sleep 0.5
+play :d4, sustain: 4
+sleep 4
+play :e4
+sleep 0.5
+play :d4
+sleep 0.5
+play :e4, sustain: 2.5
+sleep 2.5
+play :f4
+sleep 0.45
+play :e4
+sleep 0.45
+play :g4
+sleep 0.45
+play :f4
+sleep 0.45
+play :a4
+sleep 0.45
+play :c4, sustain: 1
+sleep 1
+play :a4, amp: 0.8
+sleep 0.5
+play :e4, amp: 0.7
+sleep 0.5
+play :d4, amp: 0.6
+sleep 0.5
+play :e4, amp: 0.5
